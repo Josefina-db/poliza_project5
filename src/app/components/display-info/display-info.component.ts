f6e10d9b01c42f0ff4b888753d536a4459650ae4
Dhereck1 <82067086+Dhereck1@users.noreply.github.com>
2022-02-07 15:13:03 -0300

    displayInfo working

--- src/app/components/display-info/display-info.component.ts
+++ src/app/components/display-info/display-info.component.ts
@@ -1,21 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { RestService } from 'src/app/rest.service';
-
-export interface analyzedPolicy {
-  file: string;
-  analysis: {
-    model: string;
-    confidence: number;
-    fields: [
-      {
-        field: string;
-        content: string;
-        confidence: number;
-      }
-    ];
-  };
-}
-
 @Component({
   selector: 'app-display-info',
   templateUrl: './display-info.component.html',
@@ -23,28 +7,24 @@ export interface analyzedPolicy {
 })
 export class DisplayInfoComponent implements OnInit {
   constructor(private rest: RestService) {}
-  public results: any = [];
-
+  public displayedColumns: string[] = ['file', 'analysis', 'model', 'fields'];
+  public dataSourceFile: any[] = [];
   ngOnInit(): void {
-    this.loadResults();
-    console.log(this.results);
+    this.loadResults().then((value: any) => {
+      this.dataSourceFile = value;
+      console.log('line 18', this.dataSourceFile);
+    });
+    //this.dataSource=ELEMENT_DATA;
   }
-
   loadResults() {
     try {
-      this.rest
-        .get(`http://localhost:3000/results`)
-        .subscribe((response: any) => {
-          console.log(response);
-          response.forEach((analisis: any) => {
-            this.results.push(analisis);
-          });
-          console.log(this.results[0]);
-        });
+      return this.rest.get(`http://localhost:3000/results`).toPromise();
     } catch (e) {
       console.log('ERROR', e);
+      throw e;
     }
   }
-  displayedColumns: string[] = ['file'];
-  dataSource = this.results;
+  toJson(object: any) {
+    return JSON.stringify(object);
+  }
 }
